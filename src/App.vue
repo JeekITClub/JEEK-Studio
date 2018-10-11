f9905496d57ce0d1c9b93de8631ee1c74f70239f
HarveyQiu <qiushimao@hotmail.com>
2018-10-11 08:50:33 +0800

    add vue-fullpage.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <fullpage :options="options">
+      <div class="section">
+        
+      </div>
+      <div class="section">
+      </div>
+    </fullpage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Fullpage from '';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FullPage
+  },
+  data () {
+    return {
+      options: {
+        paddingTop: '30px'
+      }
+    }
   }
 }
 </script>
